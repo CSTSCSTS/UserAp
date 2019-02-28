84db117c2a86e97a2a19df120aed4440806e79dd
OWNER <OWNER@LAPTOP-5MML4TQP>
2019-02-28 23:26:11 +0900

    セッションタイムアウトを確かめるため、テストケースを追加

--- src/test/java/com/example/demo/service/BetTest.java
+++ src/test/java/com/example/demo/service/BetTest.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.demo.constants.PokerConstants;
+import com.example.demo.controller.PokerController;
 import com.example.demo.dbflute.exbhv.PokerUserInfoBhv;
 import com.example.demo.dbflute.exbhv.PossessionMoneyBhv;
 import com.example.demo.dbflute.exentity.PokerUserInfo;
@@ -20,6 +22,7 @@ import com.example.demo.domain.model.PokerPlayingInfo;
 import com.example.demo.domain.model.PokerPlayingInfo.Winner;
 import com.example.demo.domain.model.User;
 import com.example.demo.exception.IllegalBetException;
+import com.example.demo.exception.LoginSessionTimeOutException;
 import com.example.demo.exception.UserNameDuplicateException;
 import com.example.demo.repository.MoneyRepository;
 import com.example.demo.repository.UserRepository;
@@ -27,6 +30,9 @@ import com.example.demo.repository.UserRepository;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class BetTest {
+
+	@Autowired
+	public PokerController pokerController;
 
 	@Autowired
 	public PokerService pokerService;
@@ -50,13 +56,14 @@ public class BetTest {
 	public PossessionMoneyBhv possessionMoneyBhv;
 
 	@Test
-	public void success() throws UserNameDuplicateException, IllegalBetException {
-		 PokerUserInfo entity = userSetUp(LocalDateTime.now());
-   PokerPlayingInfo info = pokerService.pokerPrepare(entity.getUserId(), new BigDecimal(100), true);
+	public void success() throws UserNameDuplicateException, IllegalBetException, LoginSessionTimeOutException {
+			PokerUserInfo entity = userSetUp(LocalDateTime.now());
+			pokerController.loginSession.setUserId(entity.getUserId());
+			pokerController.loginSession.setUserName(entity.getUserName());
+			PokerPlayingInfo info = pokerController.postPokerStart(new BigDecimal(100), true);
    assertThat(info.getDeck().size()).isEqualTo(43);
    assertThat(info.getPlayerHands().size()).isEqualTo(5);
    assertThat(info.getComputerHands().size()).isEqualTo(5);
-
 	}
 
 	@Test(expected = IllegalBetException.class)
@@ -66,35 +73,62 @@ public class BetTest {
 	}
 
 	@Test
-	public void playerWinner() throws UserNameDuplicateException {
+	public void playerWinner() throws UserNameDuplicateException, LoginSessionTimeOutException {
 		 LocalDateTime firstTime = LocalDateTime.of(2018, 12, 11, 2, 33);
 		 PokerUserInfo entity = userSetUp(firstTime);
-	  moneyService.update(entity.getUserId(), new BigDecimal(100), Winner.PLAYER);
+		 pokerController.loginSession.setUserId(entity.getUserId());
+			pokerController.loginSession.setUserName(entity.getUserName());
+			pokerController.result(new BigDecimal(100), Winner.PLAYER);
 	  Money money = moneyRepository.getMoney(entity.getUserId());
 	  assertThat(money.getMoney()).isEqualTo(new BigDecimal(1100));
 	  assertThat(money.getUpdateDate()).isNotEqualTo(firstTime);
 	}
 
 	@Test
-	public void CPUWinner() throws UserNameDuplicateException {
+	public void CPUWinner() throws UserNameDuplicateException, LoginSessionTimeOutException {
 		 LocalDateTime firstTime = LocalDateTime.of(2018, 12, 11, 2, 33);
 		 PokerUserInfo entity = userSetUp(firstTime);
-		 moneyService.update(entity.getUserId(), new BigDecimal(100), Winner.CPU);
+		 pokerController.loginSession.setUserId(entity.getUserId());
+			pokerController.loginSession.setUserName(entity.getUserName());
+			pokerController.result(new BigDecimal(100), Winner.CPU);
 		 Money money = moneyRepository.getMoney(entity.getUserId());
 		 assertThat(money.getMoney()).isEqualTo(new BigDecimal(900));
 		 assertThat(money.getUpdateDate()).isNotEqualTo(firstTime);
 	}
 
 	@Test
-	public void draw() throws UserNameDuplicateException {
+	public void draw() throws UserNameDuplicateException, LoginSessionTimeOutException {
 		 LocalDateTime firstTime = LocalDateTime.of(2018, 12, 11, 2, 33);
 		 PokerUserInfo entity = userSetUp(firstTime);
-	  moneyService.update(entity.getUserId(), new BigDecimal(100), Winner.NOTHING);
+		 pokerController.loginSession.setUserId(entity.getUserId());
+			pokerController.loginSession.setUserName(entity.getUserName());
+			pokerController.result(new BigDecimal(100), Winner.NOTHING);
 	  Money money = moneyRepository.getMoney(entity.getUserId());
 	  assertThat(money.getMoney()).isEqualTo(new BigDecimal(1000));
 	  assertThat(money.getUpdateDate()).isNotEqualTo(firstTime);
 	}
 
+	@Test(expected = LoginSessionTimeOutException.class)
+	public void getMoneySessionTimeOut() throws LoginSessionTimeOutException {
+			pokerController.getMoney();
+	}
+
+	@Test(expected = LoginSessionTimeOutException.class)
+	public void betSessionTimeOut() throws UserNameDuplicateException, LoginSessionTimeOutException, IllegalBetException {
+			pokerController.postPokerStart(new BigDecimal(100), true);
+	}
+
+	@Test(expected = LoginSessionTimeOutException.class)
+	public void handChangeSessionTimeOut() throws IOException, LoginSessionTimeOutException {
+			// 本来は、山札やプレイヤーの手札をJSONにした値が引数にわたってくるが、
+			// セッションタイムアウトになることを確かめたいだけなので、""を入れておく
+			pokerController.handChange("", "", "");
+	}
+
+	@Test(expected = LoginSessionTimeOutException.class)
+	public void moneyUpdateSessionTimeOut() throws LoginSessionTimeOutException {
+	  pokerController.result(new BigDecimal(100), Winner.PLAYER);
+	}
 
 	private PokerUserInfo userSetUp(LocalDateTime time) throws UserNameDuplicateException {
 		 if(userRepository.getPokerUserByUsername("テストユーザー").isPresent()) {
