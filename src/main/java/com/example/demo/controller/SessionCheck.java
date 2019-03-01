dfb977fa9b453a6b31412306633848db882561ab
OWNER <OWNER@LAPTOP-5MML4TQP>
2019-03-01 21:11:15 +0900

    セッションチェックをする継承元クラスを作成

---
+++ src/main/java/com/example/demo/controller/SessionCheck.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+import com.example.demo.domain.model.LoginSession;
+import com.example.demo.exception.LoginSessionTimeOutException;
+
+public class SessionCheck{
+
+		@Autowired
+		public LoginSession loginSession;
+
+		@Autowired
+		protected MessageSource messageSource;
+
+		public void sessionCheck() throws LoginSessionTimeOutException {
+				if(!loginSession.getUserId().isPresent() || !loginSession.getUserName().isPresent()) {
+		    throw new LoginSessionTimeOutException(messageSource.getMessage("login.session.timeout", null, Locale.JAPAN));
+		  }
+		}
+
+}
